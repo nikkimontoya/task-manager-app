1c58fb3b88febfd984cdac6649e69116feff565f
sviridov <sviridov.dev@gmail.com>
2022-01-25 22:42:29 +0300

    register

--- src/app/auth/components/register/register.component.ts
+++ src/app/auth/components/register/register.component.ts
@@ -1,15 +1,45 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import {HttpErrorResponse} from '@angular/common/http';
+import {UserService} from '../../../shared/services/user.service';
+import {Router} from '@angular/router';
 
 @Component({
-  selector: 'tm-register',
-  templateUrl: './register.component.html',
-  styleUrls: ['./register.component.scss']
+    selector: 'tm-register',
+    templateUrl: './register.component.html',
+    styleUrls: ['./register.component.scss']
 })
 export class RegisterComponent implements OnInit {
+    form: FormGroup;
+    error: string;
 
-  constructor() { }
+    constructor(private fb: FormBuilder, private userService: UserService, private router: Router) {}
 
-  ngOnInit(): void {
-  }
+    ngOnInit(): void {
+        this.initForm();
+    }
 
+    private initForm(): void {
+        this.form = this.fb.group({
+            username: ['', Validators.required],
+            email: ['', [Validators.required, Validators.email]],
+            password: ['', Validators.required]
+        });
+    }
+
+    submit() {
+        this.error = null;
+        if (!this.form.valid) {
+            return;
+        }
+
+        this.userService.register(this.form.value).subscribe({
+            next: () => {
+                this.router.navigateByUrl('/');
+            },
+            error: (error: HttpErrorResponse) => {
+                this.error = error.error.message;
+            }
+        });
+    }
 }
--- src/app/shared/services/user.service.ts
+++ src/app/shared/services/user.service.ts
@@ -1,9 +1,10 @@
 import {Injectable} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import {environment} from '../../../environments/environment';
-import {LoginInterface} from '../../auth/types/login.interface';
+import {LoginInterface} from '../types/login.interface';
 import {UserInterface} from '../types/user.interface';
 import {Observable, Subject, tap} from 'rxjs';
+import {RegisterInterface} from '../types/register.interface';
 
 @Injectable({
     providedIn: 'root'
@@ -13,7 +14,13 @@ export class UserService {
 
     constructor(private http: HttpClient) {}
 
-    register() {}
+    register(data: RegisterInterface) {
+        return this.http.post<UserInterface>(`${environment.apiUrl}/auth/register`, data).pipe(
+            tap((user: UserInterface) => {
+                this.$currentUser.next(user);
+            })
+        );
+    }
 
     login(data: LoginInterface): Observable<UserInterface> {
         return this.http.post<UserInterface>(`${environment.apiUrl}/auth/login`, data).pipe(
